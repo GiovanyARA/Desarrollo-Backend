4fb0d24e868306c51a0f7cb477cafccdb933b5ce
GiovanyARA <giovanyaralonso@gmail.com>
2025-09-07 19:39:02 -0600

    feat(productos): ViewSet con soporte de ordenamiento

--- productos/views.py
+++ productos/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, filters
+from .models import Producto
+from .serializers import ProductoSerializer
+
+class ProductoViewSet(viewsets.ModelViewSet):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+
+    # Ordenamiento: ?ordering=precio  | ?ordering=-precio  | ?ordering=nombre
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['precio', 'stock', 'nombre', 'creado', 'actualizado']
+    ordering = ['-creado']  # igual que Meta.ordering del modelo
